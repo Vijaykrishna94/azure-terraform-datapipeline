e7b79f0c0b25c5a6dc1068c7b999a1585c3089ac
Vijay Burra <vijay.burra@datatalks.se>
2025-01-19 21:58:04 +0100

    restructured

---
+++ providers.tf
@@ -0,0 +1,29 @@
+
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    databricks = {
+      source = "databricks/databricks"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "vj-rcm-dev-rg"
+    storage_account_name = "vjrcmdevstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
