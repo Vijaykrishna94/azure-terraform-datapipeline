64cba2870f05deb26e0239faeac6c6330ca0425b
Vijay Burra <vijay.burra@datatalks.se>
2025-01-19 22:21:18 +0100

    fixed

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">= 3.7.0"
     }
     databricks = {
       source = "databricks/databricks"
@@ -25,5 +25,6 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  use_oidc = true
   features {}
 }
