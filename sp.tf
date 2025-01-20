90d19c9954ae8e34847775ae1e351eedc2510360
Vijay Burra <vijay.burra@datatalks.se>
2025-01-20 17:35:48 +0100

    creating key vault ACP for adf

--- sp.tf
+++ sp.tf
@@ -3,9 +3,10 @@ data "azuread_client_config" "ad_current" {}
 
 resource "azuread_application_registration" "rcm_adf_app" {
   display_name = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-adf-app"
+  
 }
 
-resource "azuread_service_principal" "azuread_sp" {
+resource "azuread_service_principal" "azure_adf_sp" {
   client_id = azuread_application_registration.rcm_adf_app.client_id
   use_existing   = true
 }
