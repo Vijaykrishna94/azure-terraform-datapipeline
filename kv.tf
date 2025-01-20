c13ced352ed0f25a5844dce60be94c732ab2038e
Vijay Burra <vijay.burra@datatalks.se>
2025-01-20 17:56:53 +0100

    fixing

--- kv.tf
+++ kv.tf
@@ -32,16 +32,16 @@ resource "azurerm_key_vault" "rcm_kv" {
 
 
 
-resource "azurerm_key_vault_access_policy" "rcm-adf-principal" {
-  key_vault_id = azurerm_key_vault.rcm_kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azuread_service_principal.azuread_sp.object_id
+# resource "azurerm_key_vault_access_policy" "rcm-adf-principal" {
+#   key_vault_id = azurerm_key_vault.rcm_kv.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = azuread_service_principal.azuread_sp.object_id
 
-  key_permissions = [
-    "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge"
-  ]
-  secret_permissions = [
-    "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"
-  ]
+#   key_permissions = [
+#     "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge"
+#   ]
+#   secret_permissions = [
+#     "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"
+#   ]
 
-}
+# }
