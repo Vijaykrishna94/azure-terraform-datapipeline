841f2da9f677bee07e8000f9939331b6718e822a
Vijay Burra <vijay.burra@datatalks.se>
2025-01-20 13:21:31 +0100

    debugging_3

--- sp.tf
+++ sp.tf
@@ -1,1 +1,9 @@
+data "azuread_client_config" "ad_current" {}
+
+
+
+resource "azuread_application" "rcm_adf_app" {
+  display_name = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-adf-app"
+  owners       = [data.azuread_client_config.ad_current.object_id]
+}
 
