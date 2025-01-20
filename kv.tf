4251f3b0d44326d8b743d0b06b023e6fc4125134
Vijay Burra <vijay.burra@datatalks.se>
2025-01-20 09:12:52 +0100

    added : kv : params

--- kv.tf
+++ kv.tf
@@ -2,8 +2,8 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "rcm_kv" {
   name                        = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-kv"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.rcm_rg.location
+  resource_group_name         = azurerm_resource_group.rcm_rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
