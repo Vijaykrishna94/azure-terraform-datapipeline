a857f903820811d1397d1051d332460979fd853e
Vijay Burra <vijay.burra@datatalks.se>
2025-01-21 21:08:46 +0100

    adding kv ls

--- adf.tf
+++ adf.tf
@@ -5,16 +5,38 @@
 
 
 
+
+
+# azure keyvault linked service
+resource "azurerm_data_factory_linked_service_key_vault" "rcm_kv_ls" {
+  name            = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-kv"
+  data_factory_id = azurerm_data_factory.rcm_adf.id
+  key_vault_id    = azurerm_key_vault.rcm_kv.id
+}
+
+
+# resource "azurerm_data_factory_linked_service_sql_server" "rcm_sqldb_ls" {
+#   name            = "example"
+#   data_factory_id = azurerm_data_factory.rcm_adf.id
+
+#   connection_string = "Integrated Security=False;Data Source=test;Initial Catalog=test;User ID=test;"
+#   key_vault_password {
+#     linked_service_name = azurerm_data_factory_linked_service_key_vault.rcm_kv_ls.name
+#     secret_name         = "vj-sqldb-access-key-dev"
+#   }
+# }
+
+
 # Get the kv id  
-data "azurerm_key_vault" "current_adls_key" {
+data "azurerm_key_vault" "current_key" {
   name                = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-kv"
   resource_group_name = azurerm_resource_group.rcm_rg.name
 }
 
-#Get the secret of id
+#Get the secret of adls
 data "azurerm_key_vault_secret" "current_adls_secret" {
   name         = "vj-adls-access-key-dev"
-  key_vault_id = data.azurerm_key_vault.current_adls_key.id
+  key_vault_id = data.azurerm_key_vault.current_key.id
 }
 
 
