c8c5a6d956a9202849593443da9efc78c744c369
Vijay Burra <vijay.burra@datatalks.se>
2025-01-23 22:08:15 +0100

    refreshing storage

--- main.tf
+++ main.tf
@@ -17,13 +17,13 @@ resource "azurerm_storage_account" "rcm_adls" {
 
 
 
-# # Setting up containers
-# resource "azurerm_storage_container" "rcm_container" {
-#   for_each              = toset(["configs", "landing", "bronze", "silver", "gold", "tfstate"])
-#   name                  = each.key
-#   storage_account_name  = azurerm_storage_account.rcm_adls.name
-#   container_access_type = "private"
-# }
+# Setting up containers
+resource "azurerm_storage_container" "rcm_container" {
+  for_each              = toset(["configs", "landing", "bronze", "silver", "gold", "tfstate"])
+  name                  = each.key
+  storage_account_id = azurerm_storage_account.rcm_adls.id
+  container_access_type = "private"
+}
 
 
 
