29e398fd03d148861744f34e2ccc1cd1f9b6ce5f
Vijay Burra <vijay.burra@datatalks.se>
2025-01-23 21:47:24 +0100

    fixing storage

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_account" "rcm_adls" {
 resource "azurerm_storage_container" "rcm_container" {
   for_each              = toset(["configs", "landing", "bronze", "silver", "gold", "tfstate"])
   name                  = each.key
-  storage_account_id  = azurerm_storage_account.rcm_adls.id
+  storage_account_name  = azurerm_storage_account.rcm_adls.name
   container_access_type = "private"
 }
 
