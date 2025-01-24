f05e42a0907c81f348f116c61f35223a9f1bfb00
Vijay Burra <vijay.burra@datatalks.se>
2025-01-24 11:47:25 +0100

    commenting pass

--- sp.tf
+++ sp.tf
@@ -42,16 +42,16 @@ resource "azuread_service_principal" "rcm_adb_sp" {
 }
 
 
-#creating adb rotation policy for key
-resource "time_rotating" "month" {
-  rotation_days = 45
-}
+# #creating adb rotation policy for key
+# resource "time_rotating" "month" {
+#   rotation_days = 45
+# }
 
 
-resource "azuread_service_principal_password" "rcm_adb_pass" {
-  service_principal_id = azuread_service_principal.rcm_adb_sp.client_id
-  rotate_when_changed  = { rotation = time_rotating.month.id }
-}
+# resource "azuread_service_principal_password" "rcm_adb_pass" {
+#   service_principal_id = azuread_service_principal.rcm_adb_sp.client_id
+#   rotate_when_changed  = { rotation = time_rotating.month.id }
+# }
 
 # # Mapping azuread-adb sp
 
