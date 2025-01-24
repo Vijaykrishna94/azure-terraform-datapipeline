1d942715dfad46d486165955e29092973b7981d3
Vijay Burra <vijay.burra@datatalks.se>
2025-01-24 21:50:17 +0100

    modified db provider

--- adb.tf
+++ adb.tf
@@ -1,7 +1,6 @@
-// create PAT token to provision entities within workspace
 # resource "databricks_token" "pat" {
-
+#   provider = databricks.vj_rcm_ws
 #   comment  = "Terraform Provisioning"
-    // 100 day token
+#   // 100 day token
 #   lifetime_seconds = 8640000
 # }
