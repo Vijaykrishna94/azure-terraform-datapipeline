a0809d5642c80c0dcddbfad08d93d23d21ef559d
Vijay Burra <vijay.burra@datatalks.se>
2025-01-24 19:45:29 +0100

    refreshing values

--- sp.tf
+++ sp.tf
@@ -27,36 +27,36 @@ resource "azuread_service_principal" "azure_adls_sp" {
 
 
 
-# #Creating  azure adb app
+#  #Creating  azure adb app
 
-resource "azuread_application_registration" "rcm_adb_app" {
-  display_name = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-adb-app"
-}
+# resource "azuread_application_registration" "rcm_adb_app" {
+#   display_name = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-adb-app"
+# }
 
 
-# creating adb  azure ad sp
-resource "azuread_service_principal" "rcm_adb_sp" {
-  client_id = azuread_application_registration.rcm_adb_app.client_id
+# # creating adb  azure ad sp
+# resource "azuread_service_principal" "rcm_adb_sp" {
+#   client_id = azuread_application_registration.rcm_adb_app.client_id
   
   
-}
+# }
 
 
-#creating adb rotation policy for key
-resource "time_rotating" "month" {
-  rotation_days = 45
-}
+# #creating adb rotation policy for key
+# resource "time_rotating" "month" {
+#   rotation_days = 45
+# }
 
 
-resource "azuread_service_principal_password" "rcm_adb_pass" {
-  service_principal_id = azuread_service_principal.rcm_adb_sp.id
-  rotate_when_changed  = { rotation = time_rotating.month.id }
-}
+# resource "azuread_service_principal_password" "rcm_adb_pass" {
+#   service_principal_id = azuread_service_principal.rcm_adb_sp.id
+#   rotate_when_changed  = { rotation = time_rotating.month.id }
+# }
 
-# Mapping (Registering) azuread-db sp
+# # Mapping (Registering) azuread-db sp
 
-resource "databricks_service_principal" "rcm_db_sp" {
-  application_id = azuread_application.rcm_adb_app.client_id
-  display_name   = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-azureadb-app"
-}
+# resource "databricks_service_principal" "rcm_db_sp" {
+#   application_id = azuread_application.rcm_adb_app.client_id
+#   display_name   = "${var.resource_group_name_prefix}-${var.proj_name_prefix}-${var.env_prefix}-azureadb-app"
+# }
 
