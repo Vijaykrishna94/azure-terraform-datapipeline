9a42d791c62916279355e25fe10e41d8816e118a
Vijay Burra <vijay.burra@datatalks.se>
2025-01-25 18:34:43 +0100

    setting up cluster

--- variables.tf
+++ variables.tf
@@ -35,3 +35,23 @@ variable "admin_password" {
   sensitive   = true
   default     = null
 }
+
+
+
+variable "cluster_name" {
+  description = "A name for the cluster."
+  type        = string
+  default     = "${var.resource_group_name_prefix}${var.proj_name_prefix}${var.env_prefix}cluster"
+}
+
+variable "cluster_autotermination_minutes" {
+  description = "How many minutes before automatically terminating due to inactivity."
+  type        = number
+  default     = 60
+}
+
+variable "cluster_num_workers" {
+  description = "The number of workers."
+  type        = number
+  default     = 1
+}
