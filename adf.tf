c8081e93895e1082679c2eefc1824c1581ab6874
Vijay Burra <vijay.burra@datatalks.se>
2025-01-28 00:55:10 +0100

    testing

--- adf.tf
+++ adf.tf
@@ -431,7 +431,7 @@ resource "azurerm_data_factory_pipeline" "vj_rcm_active_tables_pl" {
 resource "azapi_update_resource" "vj_rcm_active_tables_pl_update" {
   type      = "Microsoft.DataFactory/factories/pipelines@2018-06-01"
   name      = azurerm_data_factory_pipeline.vj_rcm_active_tables_pl.name
-  parent_id = azurerm_data_factory.rcm_adf.id
+  parent_id = azurerm_data_factory_pipeline.vj_rcm_active_tables_pl.id
 
 
   body = {
